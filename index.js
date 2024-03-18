0851f696ca6d2a53df30982c8fccb90bd6e74e87
sidhant-sriv <sidhant.sriv@gmail.com>
2024-03-18 11:22:38 +0530

    Update PORT variable in index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 app.get("/form", (req, res) => {
   res.sendFile(path.join(staticDir, "form.html"));
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
